3a6d5fd53e1f8e6cdc25aff9a59f9823e6d0cca4
praveen-ncompass <praveen@ncompass.in>
2021-10-25 14:31:10 +0530

    Added gradient in header on changing organization

--- apps/web/customer/src/app/dashboard/header.tsx
+++ apps/web/customer/src/app/dashboard/header.tsx
@@ -5,36 +5,44 @@ import landingPageStyles from './dashboard.module.less'
 import { OrgnizationModel } from './model'
 import { fetchOrganizationApi } from './api'
 
-
 const DashBoardHeader = () => {
   const { Header } = Layout
   const { Option } = Select
   const [selectedOrgTheme, setSelectedTheme] = useState('')
   const [orgList, setOrgList] = useState<OrgnizationModel[]>([])
   const [selectedOrg, setSelectedOrg] = useState<string>('')
+  
   const getOrgDetails = (data: string) => {
     const getSelectedOrgData = orgList.filter((orgSelected) => orgSelected.orgId === data)
     const selectedOrg = { ...getSelectedOrgData }
-    setSelectedTheme('#' + selectedOrg[0].theme)
+    setSelectedTheme(
+        `transparent linear-gradient(90deg, 
+          #${selectedOrg[0].theme} 0%,  #${selectedOrg[0].theme} 100%) 0% 0% no-repeat padding-box`
+      )
     setSelectedOrg(selectedOrg[0].orgShortName)
   }
   const fetchOrg = async () => {
     const response = await fetchOrganizationApi()
     setOrgList(response.data)
     setSelectedOrg(response.data[0].orgShortName)
-    setSelectedTheme('#' + response.data[0].theme)
+    setSelectedTheme(
+      `transparent linear-gradient(90deg,
+         #${response.data[0].theme} 0%, #${response.data[0].theme} 
+         100%) 0% 0% no-repeat padding-box`
+    )
+    console.log(selectedOrgTheme)
+    // transparent linear-gradient(90deg, #315B81 0%, #003463 100%) 0% 0% no-repeat padding-box
   }
 
   useEffect(() => {
     fetchOrg()
   }, [])
   return (
-    <Header style={{ backgroundColor: `${selectedOrgTheme}` }} className="header">
+    <Header style={{ background: `${selectedOrgTheme}` }} className="header">
       <Select
-      style={{ backgroundColor: `${selectedOrgTheme}`, width: 360}}
+        style={{ background: `${selectedOrgTheme}`, width: 360 }}
         className={landingPageStyles.dropDown}
         onChange={getOrgDetails}
-        defaultValue={orgList.length>0 ? orgList[0].orgShortName:""}
         bordered={false}
         value={selectedOrg}
       >
@@ -46,4 +54,4 @@ const DashBoardHeader = () => {
   )
 }
 
-export {DashBoardHeader}
+export { DashBoardHeader }
--- apps/web/customer/src/app/organization/create/api.ts
+++ apps/web/customer/src/app/organization/create/api.ts
@@ -1,17 +1,17 @@
-import { apiRequest } from "../../../services/axios/axios"
-const createOrganizationApi = async(orgName:string, shortName:string, theme:string) => {
-    const dataToserver = {
-        orgName:orgName,
-        orgShortName:shortName,
-        theme
-    }
-    console.log(dataToserver)
-    const response = await apiRequest("POST",'organizations/create',dataToserver)
-    return response
+import { apiRequest } from '../../../services/axios/axios'
+const createOrganizationApi = async (orgName: string, shortName: string, theme: string) => {
+  const dataToserver = {
+    orgName: orgName,
+    orgShortName: shortName,
+    theme,
+  }
+  console.log(dataToserver)
+  const response = await apiRequest('POST', 'organizations/create', dataToserver)
+  return response
 }
 
-const getThemeApi = async() => {
-    return await apiRequest("GET","organizations/fetchThemes",'')
+const getThemeApi = async () => {
+  return await apiRequest('GET', 'organizations/fetchThemes', '')
 }
 
-export {createOrganizationApi, getThemeApi}
+export { createOrganizationApi, getThemeApi }
