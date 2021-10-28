ad5f8727f2823253809dc065d88d1f902a063a55
praveen-ncompass <praveen@ncompass.in>
2021-10-28 11:12:32 +0530

    Added invite in manage contact

--- apps/web/customer/src/app/routing/index.tsx
+++ apps/web/customer/src/app/routing/index.tsx
@@ -6,6 +6,7 @@ import Dashboard from '../dashboard/layout'
 import Otp from '../otp/otp'
 import { CreateOrg } from '../create-org/create-org'
 import { GlobalRouterPath } from '../routing/constant/globalRoute'
+import {Invite} from '../invite/invite'
 
 const AppRouter = () => {
   return (
@@ -15,7 +16,7 @@ const AppRouter = () => {
         <Route exact path={GlobalRouterPath.CREATE_ACCOUNT} component={CreateAccount}></Route>
         <Route exact path={GlobalRouterPath.OTP} component={Otp}></Route>
         <Route exact path={GlobalRouterPath.CREATE_ORG} component={CreateOrg}></Route>
-        <Route exact path={GlobalRouterPath.INVITE} component={CreateOrg}></Route>
+        <Route exact path={GlobalRouterPath.INVITE} component={Invite}></Route>
         <Route  path={GlobalRouterPath.DASHBOARD} component={Dashboard}></Route>
       </Switch>
     </Router>
